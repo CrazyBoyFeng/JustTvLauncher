7ac4cad35a05abb578f784abc8d8f99ddb84378a
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-18 19:12:19 +0800

    Fix `openCount` not sum

--- app/src/main/kotlin/com/github/crazyboyfeng/justTvLauncher/model/ShortcutGroup.kt
+++ app/src/main/kotlin/com/github/crazyboyfeng/justTvLauncher/model/ShortcutGroup.kt
@@ -1,28 +1,34 @@
 package com.github.crazyboyfeng.justTvLauncher.model
 
-class ShortcutGroup(val category: String?, val shortcutList: MutableList<Shortcut>) {
-    var openCount = 0
+class ShortcutGroup(val category: String, val shortcutList: MutableList<Shortcut>) {
+    var openCount = shortcutList.sumOf { it.openCount }
     fun add(element: Shortcut) {
-        val oc = element.openCount
-        val index = findIndex(oc, 0, shortcutList.size - 1)
+        openCount += element.openCount
+//        Log.d(TAG,"openCount[${element.id}]=${element.openCount}")
+        val index = findIndex(element.openCount, 0, shortcutList.size - 1)
+//        Log.d(TAG,"list[$index]=${element.openCount}")
         shortcutList.add(index, element)
     }
 
     private fun findIndex(oc: Int, leftIndex: Int, rightIndex: Int): Int {
         return when {
-            shortcutList[leftIndex].openCount < oc -> {
+            oc > shortcutList[leftIndex].openCount -> {
+//                Log.d(TAG,"$oc>list[left=$leftIndex]=${shortcutList[leftIndex].openCount}")
                 leftIndex
             }
             shortcutList[rightIndex].openCount > oc -> {
-                rightIndex
+//                Log.d(TAG,"list[right=$rightIndex]=${shortcutList[rightIndex].openCount}>$oc")
+                rightIndex + 1
             }
             else -> {
                 val middleIndex = (leftIndex + rightIndex) / 2
                 when {
                     shortcutList[middleIndex].openCount > oc -> {
+//                        Log.d(TAG,"list[middle=$middleIndex]>$oc")
                         findIndex(oc, leftIndex, middleIndex)
                     }
                     shortcutList[middleIndex].openCount < oc -> {
+//                        Log.d(TAG,"$oc>list[middle=$middleIndex]")
                         findIndex(oc, middleIndex + 1, rightIndex)
                     }
                     else -> {
@@ -32,4 +38,7 @@ class ShortcutGroup(val category: String?, val shortcutList: MutableList<Shortcu
             }
         }
     }
+//    companion object {
+//        private const val TAG = "ShortcutGroup"
+//    }
 }
