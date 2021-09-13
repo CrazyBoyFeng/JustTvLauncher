3ee7d74e9d3a0599761161b34441020d5e291397
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-09-14 00:45:59 +0800

    Upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.1")
+        classpath("com.android.tools.build:gradle:7.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -14,5 +14,4 @@ buildscript {
 
 tasks.wrapper{
     enabled = false
-    gradleVersion = "7.1.1"
 }
